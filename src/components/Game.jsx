87cf61216c8a7dc3f122e1175a5aed131fb3397b
Derrick Hinh <derhin1@gmail.com>
2022-04-04 13:43:25 -0500

    Added key listener

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,22 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import bat from "../assets/batFlap.gif";
 import Board from "./Board";
 const Game = () => {
   const [tickSpeed, setTickSpeed] = useState(1);
   const [score, setScore] = useState(0);
+  const [batPosition, setBatPosition] = useState({ row: 5, col: 5 });
 
   function handleKeyPress(event) {
-    if (event.keyCode === 38) {
-      alert("hello");
+    if (event.key === " ") {
+      batPosition.row++;
     }
   }
 
+  useEffect(() => {
+    window.addEventListener("keydown", (event) => {
+      handleKeyPress(event);
+    });
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
   return (
-    <>
+    <div className="gameArea">
       <div>{score}</div>
       {/* <Board /> */}
-      <img src={bat} alt="sprite" />
-    </>
+      {/* <input type="text" value={score} onKeyPress={handleKeyPress} /> */}
+      <img src={bat} alt="sprite" onKeyPress={handleKeyPress} />
+    </div>
   );
 };
 
